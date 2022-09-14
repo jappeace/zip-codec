0b01f2ec3f67abb31091b625b1eb4945e7e97a5b
Jappie Klooster <jappieklooster@hotmail.com>
2022-09-14 13:51:35 -0400

    Show multiple files benchmark
    we're slightly faster (codec is this library):
    
    benchmarking multiplefiles/unarchive/codec
    time                 27.55 ms   (26.92 ms .. 28.18 ms)
                         0.998 R²   (0.996 R² .. 0.999 R²)
    mean                 26.46 ms   (26.01 ms .. 26.85 ms)
    std dev              918.8 μs   (673.6 μs .. 1.310 ms)
    variance introduced by outliers: 10% (moderately inflated)
    
    benchmarking multiplefiles/unarchive/zip
    time                 28.11 ms   (27.45 ms .. 28.68 ms)
                         0.998 R²   (0.996 R² .. 0.999 R²)
    mean                 27.60 ms   (27.25 ms .. 27.98 ms)
    std dev              789.6 μs   (618.4 μs .. 1.048 ms)

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -2,10 +2,10 @@
 
 module Main where
 
+import Data.Foldable(fold)
 import qualified Data.Map as Map
-import           Control.Monad (forM_)
+import           Control.Monad (forM_, void)
 import qualified Data.ByteString.Lazy as B (hPut, pack, readFile)
-import qualified Data.ByteString as BS
 import           Data.Word (Word8)
 import           System.FilePath ((</>), (<.>))
 import           System.IO (IOMode(..), hPutStr, withFile)
@@ -15,6 +15,7 @@ import           Criterion.Main (Benchmark, bench, bgroup, defaultMain, nfIO)
 import           System.IO.Temp (withSystemTempDirectory, withTempDirectory)
 import           System.Random (getStdGen, randoms)
 
+import Data.Traversable.WithIndex
 import Control.Exception
 import qualified Data.Conduit.Combinators as CC
 import Data.Conduit ((.|), runConduitRes)
@@ -23,39 +24,75 @@ import qualified "zip-archive" Codec.Archive.Zip as A
 import qualified "zip" Codec.Archive.Zip as Zip
 
 import qualified Zip.Codec as This
+import qualified Data.ByteUnits as Byte
 
 
 main :: IO ()
 main = do
-    let sizes = [1024*1024, 10*1024*1024]    -- ^ sizes of files for benchmarking
+    let singleSizes = [
+            1024*1024
+          , 10*1024*1024
+          ]    -- ^ sizes of files for benchmarking
+    let multipleSizes = [
+          1024*1024
+          , 10*1024*1024
+          , 7*1024*1024
+          , 3*1024*1024
+          , 4*1024*1024
+          , 5*1024*1024
+          , 6*1024*1024
+          ]
 
     withSystemTempDirectory "zip-conduit" $ \dir -> do
-        prepareFiles dir sizes
-        defaultMain (prepareBench dir $ map show sizes)
+        prepareFiles dir singleSizes
+        prepareFiles dir multipleSizes
+        defaultMain $
+          -- (singles dir singleSizes) <>
+          multiples dir multipleSizes
 
+multiples :: FilePath
+           -> [Int]
+           -> [Benchmark]
+multiples dir names = [bgroup "multiplefiles"
+  [ bgroup "archive"
+             [
+             --   bench "codec"       $ nfIO $ zipCodecMultiple dir $ show <$> names
+             -- , bench "zip"         $ nfIO $ zipZipMultiple dir $ show <$> names
+             ]
+  , bgroup "unarchive"
+             [
+               bench "codec"       $ nfIO $ unZipCodecMultiple dir $ foldMap show names
+             , bench "zip"         $ nfIO $ unZipZipMultiple dir $ foldMap show names
+             ]
+  ]
+  ]
 
 -- | Prepares benchmarks.
-prepareBench :: FilePath     -- ^ the path to the directory with files
-             -> [FilePath]   -- ^ file names
+singles :: FilePath     -- ^ the path to the directory with files
+             -> [Int]        -- ^ sizes
              -> [Benchmark]
-prepareBench dir names =
+singles dir names = [bgroup "singlefiles"
     [ bgroup "archive"
              [
-             --   bgroup "zip-archive" $ b zipArchive
+               bgroup "zip-archive" $ makeBenchmark dir names zipArchive
              -- , bgroup "libZip"      $ b libZip
-             -- , bgroup "codec"       $ b zipCodec
-             -- , bgroup "zip"         $ b zipZip
+             , bgroup "codec"       $ makeBenchmark dir names zipCodec
+             , bgroup "zip"         $ makeBenchmark dir names zipZip
              ]
     , bgroup "unarchive"
              [
-               bgroup "zip-archive" $ b unZipArchive
-             , bgroup "zip-codec"   $ b unZipCodec
-             , bgroup "zip-zip"     $ b unZipZip
+               bgroup "zip-archive" $ makeBenchmark dir names unZipArchive
+             , bgroup "zip-codec"   $ makeBenchmark dir names unZipCodec
+             , bgroup "zip-zip"     $ makeBenchmark dir names unZipZip
              -- , bgroup "libZip"      $ b unLibZip -- way slower
              ]
-    ]
-  where
-    b f = map (\name -> bench name $ nfIO $ f dir name) names
+    ]]
+
+makeBenchmark :: FilePath -> [Int] -> (FilePath -> FilePath -> IO ()) -> [Benchmark]
+makeBenchmark dir sizes benchFun = map (\size ->
+                                          bench (Byte.getShortHand $ Byte.getAppropriateUnits $ Byte.ByteValue (fromIntegral size) Byte.Bytes) $
+                                            nfIO $ benchFun dir $ show size
+                                       ) sizes
 
 
 -- | Creates source files for archiving and archives with those
@@ -63,12 +100,18 @@ prepareBench dir names =
 prepareFiles :: FilePath    -- ^ the path to the directory for files
              -> [Int]       -- ^ sizes of files to create
              -> IO ()
-prepareFiles dir sizes = forM_ sizes $ \s -> do
+prepareFiles dir sizes = do
+  forM_ sizes $ \s -> do
     let path = dir </> show s
 
     createFile path s
     This.writeZipFile (path <.> "zip") $ Map.singleton "somename" $ This.readFileContent path
 
+  let path = dir </> fold names
+      names = show <$> sizes
+
+  -- for some reason zip package can't find files within a zip file within a subfolder.
+  This.writeZipFile (path <.> "zip") $ foldMap (\key -> Map.singleton key $ This.readFileContent $ dir </> key) names
 
 -- | Creates a file of specified length with random content.
 createFile :: FilePath -> Int -> IO ()
@@ -94,7 +137,7 @@ libZip dir name =
     withTempDirectory dir "libZip" $ \tmpDir ->
         L.withArchive [L.CreateFlag] (tmpDir </> name <.> "zip") $ do
            zs <- L.sourceFile (dir </> name) 0 0
-           L.addFile (dir </> name) zs
+           _ <- L.addFile (dir </> name) zs
            return ()
 
 zipCodec :: FilePath -> FilePath -> IO ()
@@ -109,6 +152,20 @@ zipZip dir name = do
       Zip.createArchive (tmpDir </> name <.> "zip") $ do
         selector <- Zip.mkEntrySelector "x"
         Zip.loadEntry Zip.Deflate selector (dir </> name)
+
+zipCodecMultiple :: FilePath -> [FilePath] -> IO ()
+zipCodecMultiple dir names = do
+    withTempDirectory dir "codec" $ \tmpDir ->
+      This.writeZipFile (tmpDir </> fold names <.> "zip") $
+        foldMap (\name -> This.readFileContentMap (dir </> name)) names
+
+zipZipMultiple :: FilePath -> [FilePath] -> IO ()
+zipZipMultiple dir names = do
+    withTempDirectory dir "zip" $ \tmpDir ->
+      Zip.createArchive (tmpDir </> fold names <.> "zip") $ do
+        forM_ names $ \name -> do
+          selector <- Zip.mkEntrySelector name
+          Zip.loadEntry Zip.Deflate selector (dir </> name)
 
 ------------------------------------------------------------------------------
 -- Exctract files from archive with three different packages.
@@ -141,6 +198,31 @@ unZipZip dir name = do
     -- then writing.
     Zip.getEntrySource $ fst $ head (Map.toList entries)
   runConduitRes $ entry.| CC.sinkFile (dir </> name)
+
+
+unZipCodecMultiple :: FilePath -> FilePath -> IO ()
+unZipCodecMultiple dir name = do
+    hashmapE <- This.readZipFile $ dir </> name <.> "zip"
+    case hashmapE of
+      Left x -> throwIO x
+      Right map' -> do
+        -- error $ show $ () <$ map'
+        forM_ map' $ \fileContent ->
+          runConduitRes $ This.fcFileContents fileContent .| CC.sinkFile (dir </> name)
+
+deriving instance Show Zip.EntryDescription
+
+unZipZipMultiple :: FilePath -> FilePath -> IO ()
+unZipZipMultiple dir name = do
+  entryMap <- Zip.withArchive (dir </> name <.> "zip") $ do
+    entries <- Zip.getEntries
+    -- using conduit is much faster then reading it all into memory and
+    -- then writing.
+    itraverse (\key _ -> Zip.getEntrySource key) entries
+
+  void $ itraverse (\key val ->
+                runConduitRes $ val .| CC.sinkFile (dir </> Zip.unEntrySelector key)
+               ) entryMap
 
 
 ------------------------------------------------------------------------------
